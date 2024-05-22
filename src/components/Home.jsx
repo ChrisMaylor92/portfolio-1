44322df1787d3aeedf937d08b2f3d27e2a2110bf
Chris Maylor <chrismaylor1@hotmail.com>
2024-05-22 12:44:12 +0100

    testing mobile view changes view work button position

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,11 @@ export const Home = () => {
     return (
         <div>
 {/* Mobile View */}
-        <div name="home" className="md:hidden w-full h-screen grid grid-rows-2 justify-center">
+        <div name="home" className="md:hidden w-full h-screen ">
             {/* container */}
             
             
-            <div className="max-w-[1500px] px-5 pt-20 flex flex-col  h-full">
+            <div className="max-w-[1500px] px-5 pt-20 flex flex-col ">
                 {/* <p className="text-[#5fff3f] font-bold pb-2 text-2xl">Hi, my name is</p> */}
                 <TypeAnimation 
                 sequence={[
@@ -83,16 +83,17 @@ export const Home = () => {
                 />
                 {/* <h2 className="text-3xl md:text-6xl font-bold text-[#8892b0]">& I'm a Full Stack Developer</h2> */}
                 {/* <p className="text-lg text-gray-300 py-4 max-w-700px">Welcome to my portfolio website, please take a look around. I built this website using Javascript, React, TailwindCSS & Adobe CSS. </p> */}
-                 <div className="pt-1">
+                
+             
+            </div>
+            <div className="pt-1 flex justify-center">
                     <Link to="work" smooth={true} duration={500} >
-                        <button className="text-white text-sm border-2 px-3 py-2 my-2  hover:bg-[#43ad2e] hover:border-[#43ad2e]">
+                        <button className="text-white text-sm border-2 px-3 py-2 my-2 hover:bg-[#43ad2e] hover:border-[#43ad2e]">
                         View Work 
                         
                         </button>
                         </Link>
                     
-                </div>
-             
             </div>
         </div> 
 {/* Desktop View */}
