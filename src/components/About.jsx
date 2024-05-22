ce7ec81a63a1a5fccd2d0e2b9aebe062c8ddfb1c
Chris Maylor <chrismaylor1@hotmail.com>
2024-05-22 16:36:26 +0100

    resizes about components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,31 @@
 export const About = () => {
     return (
-        <div name="about" className="w-full h-screen grid grid-cols-3 text-gray-300 "> 
+        <div name="about">
+        {/* Mobile View */}
+        <div  className="md:hidden w-full h-screen flex  text-gray-300 "> 
+        
+            <div className="flex flex-col justify-center items-center w-full h-full">
+            <div className="h-60">
+
+            </div>
+                <div className="max-w-[1600px] w-full grid grid-cols-3 gap-8 px-4">
+                    <div className="pb-4 ">
+                        <p className="text-2xl font-bold inline border-b-4 border-[#5fff3f]">About</p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className="max-w-[1600px] w-full px-4">
+                    
+                    <div className="">
+                        <p className="text-md">I'm a recent graduate of Northcoders Software Development Bootcamp. Currently, I'm focusing on building responsive full-stack web-applications to boost my portfolio whilst I look for work. With a background in Illustration, Animation and Graphic Design and a love for Logical Problem Solving, I plan to tie my love for design and ability to write robust code into a career as a Full Stack Software Developer.</p>
+               
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        {/* Desktop View */}
+        <div name="about" className="hidden w-full h-screen md:grid grid-cols-3 text-gray-300 "> 
             <div className="flex flex-col col-span-2 justify-center items-center w-full h-full">
                 <div className="max-w-[1600px] w-full grid grid-cols-3 gap-8 px-4">
                     <div className="sm:text-right pb-8 pl-4">
@@ -21,6 +46,7 @@ export const About = () => {
                 </div>
                 
             </div>
+        </div>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import {FaBars, FaTimes, FaFolderOpen, FaHome} from 'react-icons/fa'
+import { FaFolderOpen, FaHome} from 'react-icons/fa'
 import {FaScrewdriverWrench, FaPersonSnowboarding} from 'react-icons/fa6'
 import { IoIosMail } from "react-icons/io";
 import logo from '../assets/CMLogo.png'
-import { useState } from 'react'
+
 import { Link } from 'react-scroll';
 import { Footer } from './Footer';
 
 
 export const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  
+    
     return (
         <div>
 
@@ -49,39 +49,7 @@ export const Navbar = () => {
                     </li>
                 </ul>
             
-            {/* Hamburger */}
-                <div onClick={handleClick} className="md:hidden z-10">
-                    {!nav ? <FaBars/> : <FaTimes/> }
-                </div>
-            {/* Mobile Menu */}
-                <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#111111] flex flex-col justify-center items-center"}>
-                    <li className="py-6 text-4xl">
-                        <Link onClick={handleClick} to="home" smooth={true} duration={500} >
-                            Home
-                        </Link>
-                    </li>
-                    <li className="py-6 text-4xl">
-                        <Link onClick={handleClick} to="skills" smooth={true} duration={500} >
-                            Skills
-                        </Link>
-                    </li>
-                    <li className="py-6 text-4xl">
-                        <Link onClick={handleClick} to="about" smooth={true} duration={500} >
-                            About
-                        </Link>
-                    </li>
-                    
-                    <li className="py-6 text-4xl">
-                        <Link onClick={handleClick} to="work" smooth={true} duration={500} >
-                            Work
-                        </Link>
-                    </li>
-                    <li className="py-6 text-4xl">
-                        <Link onClick={handleClick} to="contact" smooth={true} duration={500} >
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
+         
             
             {/* scroll links */}
                 <div className="hidden lg:flex fixed flex-col top-[37%] left-0">
